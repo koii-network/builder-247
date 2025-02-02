38c0bba7a6cb45257452b7ad876c0fb385595d0f
herman124410 <herman@koii.network>
2025-02-02 11:35:57 -0400

    Remove dollar sign

--- task/constants.py
+++ task/constants.py
@@ -1,5 +1,5 @@
 PROMPTS = {
-    "setup_repository": "Please create a new branch and switch to it in the repository located at ${repo_path}. Ensure that this branch fully addresses the task described by: ${todo}", 
+    "setup_repository": "Please create a new branch and switch to it in the repository located at {repo_path}. Ensure that this branch fully addresses the task described by: {todo}", 
     "files": "Below is the list of files in the repository: {files}. To navigate to the repository path, please use the command 'cd test'.",
     "commit": "Please commit using the {repo_path} as repo_path. The information is {todo}",
     "push": "Please push using the {repo_path} as repo_path.",
