9c6e3cb321aa7badc1ff303a3319d6e9ebcbad04
herman124410 <herman@koii.network>
2025-02-03 09:39:33 -0400

    tested with more, fixed the pr request

--- task/constants.py
+++ task/constants.py
@@ -3,6 +3,6 @@ PROMPTS = {
     "files": "Below is the list of files in the repository: {files}. To navigate to the repository path, please use the command 'cd test'.",
     "commit": "Please commit using the {repo_path} as repo_path. The information is {todo}",
     "push": "Please push using the {repo_path} as repo_path.",
-    "create_pr": "Please create a pull request with the following details: todo {todo}, repo full name {repo_full_name}, head {head}, base {base}"
+    "create_pr": "Please create a pull request with the following details: todo {todo}, repo full name {repo_full_name}, head {head}, base {base}; You have to include [x] I have tested my changes in the checklist."
     
 }
