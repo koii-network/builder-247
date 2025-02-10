0c119772aef94abc17660be3143f37abaccf6be9
Laura Abro <laura@koii.network>
2025-02-10 14:22:21 -0400

    enable create todos

--- planner/src/routes.ts
+++ planner/src/routes.ts
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import { fetchTodo } from "./controllers/fetchToDo";
-// import { createTodo } from "./controllers/createToDo";
+import { createTodo } from "./controllers/createToDo";
 import { addPR } from "./controllers/addPR";
 import { checkToDo } from "./controllers/checkToDo";
 const router = Router();
 
 router.post("/fetch-to-do", fetchTodo);
-// router.post("/create-to-do", createTodo);
+router.post("/create-to-do", createTodo);
 router.post("/add-pr-to-to-do", addPR);
 router.post("/check-to-do", checkToDo);
 
