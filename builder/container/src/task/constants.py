4410729d8d2222385761f66104de977debdf0087
Laura Abro <laura@koii.network>
2025-02-11 09:08:09 -0400

    small fixes to agent

--- builder/container/src/task/constants.py
+++ builder/container/src/task/constants.py
@@ -1,6 +1,6 @@
 PROMPTS = {
-    "setup_repository": "Please create a new branch and switch to it in the repository located at {repo_path}. "
-    "Give the branch a name that describes the task: {todo}",
+    "setup_repository": "Please create a new branch off main and switch to it in the "
+    "repository located at {repo_path}. Give the branch a name that describes the task: {todo}",
     "files": "Below is the list of files in the repository: {files}. Remember every command you run is on the base "
     "directory. Please use the command 'cd {repo_path}' for every command you want to run in the repo Path.",
     "commit": "Please commit using the {repo_path} as repo_path. "
