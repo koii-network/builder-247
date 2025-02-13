e233694a10abfadba82d7ea6b15ae78773d76bfb
Laura Abro <laura@koii.network>
2025-02-13 09:14:20 -0400

    fix typescript error

--- planner/src/routes.ts
+++ planner/src/routes.ts
@@ -1,13 +1,13 @@
 import { Router } from "express";
 import { fetchTodo } from "./controllers/fetchToDo";
-// import { createTodo } from "./controllers/createToDo";
 import { addPR } from "./controllers/addPR";
 import { checkToDo } from "./controllers/checkToDo";
+import type { RequestHandler } from "express";
+
 const router = Router();
 
-router.post("/fetch-to-do", fetchTodo);
-// router.post("/create-to-do", createTodo);
-router.post("/add-pr-to-to-do", addPR);
-router.post("/check-to-do", checkToDo);
+router.post("/fetch-to-do", fetchTodo as RequestHandler);
+router.post("/add-pr-to-to-do", addPR as RequestHandler);
+router.post("/check-to-do", checkToDo as RequestHandler);
 
 export default router;
