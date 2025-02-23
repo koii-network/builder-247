bdeb97b5439b3eadd589c70303dc0d0906424592
Laura Abro <laura@koii.network>
2025-02-23 01:23:22 -0400

    handle errors

--- tasks/builder/src/task/3-audit.ts
+++ tasks/builder/src/task/3-audit.ts
@@ -9,52 +9,57 @@ export async function audit(cid: string, roundNumber: number, submitterKey: stri
    * The default implementation retrieves the proofs from IPFS
    * and sends them to your container for auditing
    */
-  console.log(`AUDIT SUBMISSION FOR ROUND ${roundNumber}`);
+  try {
+    console.log(`AUDIT SUBMISSION FOR ROUND ${roundNumber}`);
 
-  // get the submission from IPFS
-  const submissionString = await getFile(cid);
-  const submission = JSON.parse(submissionString);
-  console.log({ submission });
+    // get the submission from IPFS
+    const submissionString = await getFile(cid);
+    const submission = JSON.parse(submissionString);
+    console.log({ submission });
 
-  // verify the signature of the submission
-  const signaturePayload = await namespaceWrapper.verifySignature(submission.signature, submitterKey);
+    // verify the signature of the submission
+    const signaturePayload = await namespaceWrapper.verifySignature(submission.signature, submitterKey);
 
-  console.log({ signaturePayload });
+    console.log({ signaturePayload });
 
-  // verify the signature payload
-  if (signaturePayload.error || !signaturePayload.data) {
-    console.error("INVALID SIGNATURE");
-    return false;
+    // verify the signature payload
+    if (signaturePayload.error || !signaturePayload.data) {
+      console.error("INVALID SIGNATURE");
+      return false;
+    }
+    const data = JSON.parse(signaturePayload.data);
+
+    if (
+      data.taskId !== TASK_ID ||
+      data.roundNumber !== roundNumber ||
+      data.stakingKey !== submitterKey ||
+      !data.pubKey ||
+      !data.prUrl
+    ) {
+      console.error("INVALID SIGNATURE DATA");
+      return false;
+    }
+
+    const orca = await getOrcaClient();
+
+    // Send the submission to the ORCA container for auditing
+    const result = await orca.podCall(`audit/${roundNumber}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        submission: data,
+        signature: submission.signature,
+        stakingKey: submitterKey,
+        pubKey: data.pubKey,
+      }),
+    });
+
+    // return the result of the audit (true or false)
+    return result.data;
+  } catch (error) {
+    console.error("ERROR AUDITING SUBMISSION", error);
+    return true;
   }
-  const data = JSON.parse(signaturePayload.data);
-
-  if (
-    data.taskId !== TASK_ID ||
-    data.roundNumber !== roundNumber ||
-    data.stakingKey !== submitterKey ||
-    !data.pubKey ||
-    !data.prUrl
-  ) {
-    console.error("INVALID SIGNATURE DATA");
-    return false;
-  }
-
-  const orca = await getOrcaClient();
-
-  // Send the submission to the ORCA container for auditing
-  const result = await orca.podCall(`audit/${roundNumber}`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      submission: data,
-      signature: submission.signature,
-      stakingKey: submitterKey,
-      pubKey: data.pubKey,
-    }),
-  });
-
-  // return the result of the audit (true or false)
-  return result.data;
 }
