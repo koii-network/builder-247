bfd9dd1ba62da5904b5fd780e65fa51db6391a5d
HermanL02 <Hermanyiqunliang@gmail.com>
2025-03-20 11:59:29 -0300

    Implement existing issues check in task function
    - Add a new utility function, getExistingIssues, to check for existing issues before proceeding with task execution.
    - Comment out previous pod call logic and related code to streamline the task function.
    - Update task function to store results based on the presence of existing issues, enhancing task management flow.

--- tasks/planner/src/task/1-task.ts
+++ tasks/planner/src/task/1-task.ts
@@ -3,7 +3,7 @@ import { namespaceWrapper, TASK_ID } from "@_koii/namespace-wrapper";
 import "dotenv/config";
 import { getLeaderNode } from "../utils/leader";
 import { getDistributionList } from "../utils/distributionList";
-
+import { getExistingIssues } from "../utils/existingIssues";
 interface PodCallBody {
   taskId: string;
   roundNumber: number;
@@ -31,23 +31,31 @@ export async function task(roundNumber: number): Promise<void> {
   try {
     const orcaClient = await getOrcaClient();
 
-    await orcaClient.podCall(`create-aggregator-repo/${roundNumber + 1}`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      // TODO: Change to dynamic repo owner and name by checking the middle server
-      body: JSON.stringify({ taskId: TASK_ID, repoOwner: "koii-network", repoName: "builder-test" }),
-    });
+    // await orcaClient.podCall(`create-aggregator-repo/${roundNumber + 1}`, {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   // TODO: Change to dynamic repo owner and name by checking the middle server
+    //   body: JSON.stringify({ taskId: TASK_ID, repoOwner: "koii-network", repoName: "builder-test" }),
+    // });
 
     const stakingKeypair = await namespaceWrapper.getSubmitterAccount();
     if (!stakingKeypair) {
       throw new Error("No staking keypair found");
     }
     const stakingKey = stakingKeypair.publicKey.toBase58();
-    const pubKey = await namespaceWrapper.getMainAccountPubkey();
-    if (!pubKey) {
-      throw new Error("No public key found");
+    // const pubKey = await namespaceWrapper.getMainAccountPubkey();
+    // if (!pubKey) {
+    //   throw new Error("No public key found");
+    // }
+
+    const existingIssues = await getExistingIssues();
+    if (existingIssues.length == 0) {
+      namespaceWrapper.storeSet(`result-${roundNumber}`, "False");
+      return;
+    } else {
+      namespaceWrapper.storeSet(`result-${roundNumber}`, "True");
     }
     const { isLeader, leaderNode } = await getLeaderNode({
       roundNumber,
@@ -58,68 +66,68 @@ export async function task(roundNumber: number): Promise<void> {
     if (leaderNode === null) {
       return;
     }
-    const payload = {
-      taskId: TASK_ID,
-      roundNumber,
-      githubUsername: process.env.GITHUB_USERNAME,
-      repoOwner: leaderNode,
-      repoName: "builder-test",
-      stakingKey,
-      pubKey,
-      action: "task",
-    };
-    const stakingSignature = await namespaceWrapper.payloadSigning(payload, stakingKeypair.secretKey);
-    const publicSignature = await namespaceWrapper.payloadSigning(payload);
-    if (!stakingSignature || !publicSignature) {
-      throw new Error("Signature generation failed");
-    }
+    // const payload = {
+    //   taskId: TASK_ID,
+    //   roundNumber,
+    //   githubUsername: process.env.GITHUB_USERNAME,
+    //   repoOwner: leaderNode,
+    //   repoName: "builder-test",
+    //   stakingKey,
+    //   pubKey,
+    //   action: "task",
+    // };
+    // const stakingSignature = await namespaceWrapper.payloadSigning(payload, stakingKeypair.secretKey);
+    // const publicSignature = await namespaceWrapper.payloadSigning(payload);
+    // if (!stakingSignature || !publicSignature) {
+    //   throw new Error("Signature generation failed");
+    // }
 
-    const podCallBody: PodCallBody = {
-      taskId: TASK_ID!,
-      roundNumber,
-      stakingKey,
-      pubKey,
-      stakingSignature,
-      publicSignature,
-      repoOwner: leaderNode,
-      repoName: "builder-test",
-      distributionList: {},
-    };
-    let podCallUrl;
-    if (isLeader) {
-      podCallUrl = `leader-task/${roundNumber}`;
+    // const podCallBody: PodCallBody = {
+    //   taskId: TASK_ID!,
+    //   roundNumber,
+    //   stakingKey,
+    //   pubKey,
+    //   stakingSignature,
+    //   publicSignature,
+    //   repoOwner: leaderNode,
+    //   repoName: "builder-test",
+    //   distributionList: {},
+    // };
+    // let podCallUrl;
+    // if (isLeader) {
+    //   podCallUrl = `leader-task/${roundNumber}`;
 
-      try {
-        const distributionList = await getDistributionList(roundNumber - 3);
-        if (!distributionList || Object.keys(distributionList).length === 0) {
-          console.log("No distribution list available for this round, skipping leader task");
-          return;
-        }
+    //   try {
+    //     const distributionList = await getDistributionList(roundNumber - 3);
+    //     if (!distributionList || Object.keys(distributionList).length === 0) {
+    //       console.log("No distribution list available for this round, skipping leader task");
+    //       return;
+    //     }
 
-        try {
-          podCallBody.distributionList = distributionList;
-        } catch (parseError) {
-          console.error("Failed to parse distribution list:", parseError);
-          console.log("Raw distribution list:", distributionList);
-          console.log("Skipping leader task due to invalid distribution list");
-          return;
-        }
-      } catch (distError) {
-        console.error("Error fetching distribution list:", distError);
-        console.log("Skipping leader task due to distribution list fetch error");
-        return;
-      }
-    } else {
-      podCallUrl = `worker-task/${roundNumber}`;
-    }
+    //     try {
+    //       podCallBody.distributionList = distributionList;
+    //     } catch (parseError) {
+    //       console.error("Failed to parse distribution list:", parseError);
+    //       console.log("Raw distribution list:", distributionList);
+    //       console.log("Skipping leader task due to invalid distribution list");
+    //       return;
+    //     }
+    //   } catch (distError) {
+    //     console.error("Error fetching distribution list:", distError);
+    //     console.log("Skipping leader task due to distribution list fetch error");
+    //     return;
+    //   }
+    // } else {
+    //   podCallUrl = `worker-task/${roundNumber}`;
+    // }
 
-    await orcaClient.podCall(podCallUrl, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(podCallBody),
-    });
+    // await orcaClient.podCall(podCallUrl, {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify(podCallBody),
+    // });
   } catch (error) {
     console.error("EXECUTE TASK ERROR:", error);
   }
