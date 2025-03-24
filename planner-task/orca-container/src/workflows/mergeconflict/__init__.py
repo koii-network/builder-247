f387a79a3d2419cca1a2719e87297c136b03ca84
Laura Abro <laura@koii.network>
2025-03-24 09:09:53 -0300

    remove duplicated files

--- planner-task/orca-container/src/workflows/mergeconflict/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Merge conflict resolver workflow package."""
-
-from src.workflows.mergeconflict.workflow import MergeConflictWorkflow
-from src.workflows.mergeconflict.prompts import PROMPTS
-
-__all__ = ["MergeConflictWorkflow", "PROMPTS"]
