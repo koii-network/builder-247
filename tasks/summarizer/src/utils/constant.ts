1a1eafd1717db228d7620c771f3c883055e35865
HermanL02 <Hermanyiqunliang@gmail.com>
2025-03-25 18:09:55 -0300

    testing task done -- need to modify the constant back and config task back

--- tasks/summarizer/src/utils/constant.ts
+++ tasks/summarizer/src/utils/constant.ts
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 export const defaultRepoOwner = process.env.DEFAULT_REPO_OWNER || "koii-network";
 
-export const defaultBountyMarkdownFile = process.env.DEFAULT_BOUNTY_MARKDOWN_FILE || "https://raw.githubusercontent.com/koii-network/prometheus-swarm-bounties/master/README.md"
+export const defaultBountyMarkdownFile = process.env.DEFAULT_BOUNTY_MARKDOWN_FILE || "https://raw.githubusercontent.com/HermanL02/prometheus-swarm-bounties/master/README.md"
 
 export const status = {
   ISSUE_FAILED_TO_BE_SUMMARIZED: "Issue failed to be summarized",
