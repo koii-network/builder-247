48a1f678a8470757cc834a074a26bd53a67604e7
Laura Abro <laura@koii.network>
2025-04-03 11:42:32 -0300

    add additional status

--- server/src/models/Issue.ts
+++ server/src/models/Issue.ts
@@ -13,7 +13,8 @@ enum IssueStatus {
   INITIALIZED = "initialized",
   AGGREGATOR_PENDING = "aggregator_pending",
   IN_PROCESS = "in_process",
-  ASSIGN_PENDING = "assign_pending", // Means assigned to a leader node
+  ASSIGN_PENDING = "assign_pending", // Means awaiting assignment to a leader node
+  ASSIGNED = "assigned", // Means assigned to a leader node
   IN_REVIEW = "in_review", // Means a PR is opened and waiting for review
   APPROVED = "approved", // Means a PR passed audit and appeared on the distribution list
   MERGED = "merged", // Means a PR is merged manually
