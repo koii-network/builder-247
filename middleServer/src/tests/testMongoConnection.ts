4d2e75b30a43a3e8c506f013dfe0a50f0594ea5d
HermanL02 <Hermanyiqunliang@gmail.com>
2025-04-11 00:16:49 -0300

    fix tsc issue

--- middleServer/src/tests/testMongoConnection.ts
+++ middleServer/src/tests/testMongoConnection.ts
@@ -1,24 +1,24 @@
-import { checkConnections } from "../services/database/database";
-import { SummarizerRecordModel } from "../models/Summarizer";
-import { IssueModel } from "../models/Issue";
-import SwarmBounty from "../models/SwarmBounties";
-import { TodoModel } from "../models/Todo";
-// import Todo from "../models/Todo";
-async function testMongoConnection() {
-    await checkConnections();
-    console.log("Connected to MongoDB");
+// import { checkConnections } from "../services/database/database";
+// import { SummarizerRecordModel } from "../models/Summarizer";
+// import { IssueModel } from "../models/Issue";
+// import SwarmBounty from "../models/SwarmBounties";
+// import { TodoModel } from "../models/Todo";
+// // import Todo from "../models/Todo";
+// async function testMongoConnection() {
+//     await checkConnections();
+//     console.log("Connected to MongoDB");
 
-    // const summarizerRecord = await SummarizerRecordModel.find();
-    // console.log(summarizerRecord);
+//     // const summarizerRecord = await SummarizerRecordModel.find();
+//     // console.log(summarizerRecord);
 
-    // const issue = await IssueModel.find();
-    // console.log(issue);
+//     // const issue = await IssueModel.find();
+//     // console.log(issue);
 
-    const swarmBounty = await SwarmBounty.find();
-    console.log(swarmBounty.length);
+//     const swarmBounty = await SwarmBounty.find();
+//     console.log(swarmBounty.length);
 
-    const todo = await TodoModel.find();
-    console.log(todo.length);
-}
+//     const todo = await TodoModel.find();
+//     console.log(todo.length);
+// }
 
-testMongoConnection();
+// testMongoConnection();
