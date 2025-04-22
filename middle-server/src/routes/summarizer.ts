3891c622ee3268fc112ba771863c6656aef81593
Laura Abro <laura@koii.network>
2025-04-22 16:35:32 -0300

    rename folders

--- middle-server/src/routes/summarizer.ts
+++ middle-server/src/routes/summarizer.ts
@@ -8,12 +8,9 @@ const router = Router();
 
 /********** Worker ***********/
 // router.post("/worker/fetch-summarizer-todo", fetchSummarizerRequest as RequestHandler);
-router.post('/worker/add-pr-to-summarizer-todo', addSummarizerRequest as RequestHandler);
-router.post(
-  '/worker/trigger-fetch-audit-result',
-  triggerFetchAuditResultSummarizer as RequestHandler
-);
-router.post('/worker/check-summarizer', checkSummarizerRequest as RequestHandler);
+router.post('/worker/add-todo-pr', addSummarizerRequest as RequestHandler);
+router.post('/worker/update-audit-result', triggerFetchAuditResultSummarizer as RequestHandler);
+router.post('/worker/check-todo', checkSummarizerRequest as RequestHandler);
 // router.post("/worker/trigger-update-swarms-status", triggerUpdateSwarmsStatus as RequestHandler);
 // router.post("/worker/trigger-save-swarms-for-round", triggerSaveSwarmsForRound as RequestHandler);
 
