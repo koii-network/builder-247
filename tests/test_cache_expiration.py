973ddb909aa961332efb6c11f176f276193ca269
ChiaMiner44 <ChiaMiner44@users.noreply.github.com>
2025-05-05 15:02:09 +0000

    Update cache expiration tests for more robust key expiration checking

--- tests/test_cache_expiration.py
+++ tests/test_cache_expiration.py
@@ -56,12 +56,24 @@ def test_clear_cache():
 
 def test_get_all_valid_keys():
     """Test retrieving all valid keys in the cache"""
-    cache = ExpiringCache(default_ttl=2)
-    cache.set('key1', 'value1')
-    cache.set('key2', 'value2', ttl=1)
-    cache.set('key3', 'value3', ttl=3)
+    cache = ExpiringCache()
     
+    # Set specific keys with varying TTLs
+    cache.set('key1', 'value1', ttl=3)
+    cache.set('key2', 'value2', ttl=1)
+    cache.set('key3', 'value3', ttl=5)
+    
+    # First check: all keys should be valid
+    assert set(cache.get_all_valid_keys()) == {'key1', 'key2', 'key3'}
+    
+    # Wait just over 1 second to expire key2
+    time.sleep(1.1)
+    
+    # Second check: key2 should be removed
     assert set(cache.get_all_valid_keys()) == {'key1', 'key3'}
     
+    # Wait to expire more keys
     time.sleep(2)
+    
+    # Final check: only key3 should remain
     assert cache.get_all_valid_keys() == ['key3']
