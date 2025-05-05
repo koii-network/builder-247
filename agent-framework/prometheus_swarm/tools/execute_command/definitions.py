e7ef7e8ea1ab80f215e1aafd992d876b77f394ea
ChiaMiner44 <ChiaMiner44@users.noreply.github.com>
2025-05-05 13:57:52 +0000

    Add Dad Joke Command Handler to Execution Definitions

--- agent-framework/prometheus_swarm/tools/execute_command/definitions.py
+++ agent-framework/prometheus_swarm/tools/execute_command/definitions.py
@@ -3,6 +3,7 @@ from prometheus_swarm.tools.execute_command.implementations import (
     run_tests,
     install_dependency,
     setup_dependencies,
+    get_dad_joke,
 )
 
 
@@ -87,4 +88,13 @@ DEFINITIONS = {
         },
         "function": setup_dependencies,
     },
+    "get_dad_joke": {
+        "name": "get_dad_joke",
+        "description": "Fetch a random dad joke from the icanhazdadjoke API",
+        "parameters": {
+            "type": "object",
+            "properties": {},
+        },
+        "function": get_dad_joke,
+    }
 }
