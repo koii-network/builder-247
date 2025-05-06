bd7be5a4f51c7951f5073986e6e2621b1f9ddb37
Obludka <Obludka@users.noreply.github.com>
2025-05-06 01:18:08 +0000

    Update jest config for TypeScript testing

--- middle-server/jest.config.js
+++ middle-server/jest.config.js
@@ -1,6 +1,18 @@
 module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  testMatch: ["**/tests/**/*.test.ts"],
-  moduleFileExtensions: ["ts", "js"],
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/tests', '<rootDir>/src'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  coverageDirectory: './coverage',
+  collectCoverageFrom: [
+    'src/**/*.{js,ts}',
+    '!**/node_modules/**',
+    '!**/dist/**'
+  ],
+  coverageReporters: ['text', 'lcov'],
+  verbose: true
 };
