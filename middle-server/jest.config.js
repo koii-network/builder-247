8a564d0f3eab6cb583a7dcb503d13d35b34ae937
fantasynhi88 <fantasynhi88@users.noreply.github.com>
2025-05-06 01:18:49 +0000

    Update Jest Configuration for TypeScript

--- middle-server/jest.config.js
+++ middle-server/jest.config.js
@@ -1,6 +1,16 @@
 module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  testMatch: ["**/tests/**/*.test.ts"],
-  moduleFileExtensions: ["ts", "js"],
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/*.test.ts'],
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  collectCoverageFrom: [
+    'src/**/*.{js,ts}',
+    '!src/**/*.d.ts'
+  ],
+  clearMocks: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
 };
